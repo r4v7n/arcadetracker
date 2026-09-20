512add0b718f0bb3f598296a72997f752ae7606b
r4v7n <120674741+r4v7n@users.noreply.github.com>
2026-09-20 21:29:38 +0530

    Setting up tailwindcss.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 import process from "node:process";
 import path from "path";
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 
   resolve: {
     alias: {
